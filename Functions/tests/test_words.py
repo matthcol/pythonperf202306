5a183323f5bef05a103da24632429387d42c68b1
matthcol <matthias.colin@gmail.com>
2023-06-20 17:12:42 +0200

    file was not saved ;)

--- Functions/tests/test_words.py
+++ Functions/tests/test_words.py
@@ -10,14 +10,6 @@ def test_letters_from_one_word_to_dict():
     # then
     assert expected_letter_dict == letter_dict
 
-@pytest.fixture
-def word_toulouse():
-    return "Toulouse"
-
-@pytest.fixture
-def letters_toulouse_cs():
-    return {"T":1, "o":2, "u":2, "l":1, "s":1, "e":1}
-
 def test_letters_from_one_word_to_dict_fixture(word_toulouse, letters_toulouse_cs):
     # given: cf args    
     # when
