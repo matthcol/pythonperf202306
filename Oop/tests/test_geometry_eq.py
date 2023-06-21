c687210d20cd0db31f4c6d61358021fa2d50d5d1
matthcol <matthias.colin@gmail.com>
2023-06-21 11:06:28 +0200

    test eq for Point

---
+++ Oop/tests/test_geometry_eq.py
@@ -0,0 +1,61 @@
+import pytest
+import geometry as geo
+
+
+@pytest.mark.parametrize(
+    ["p1","p2"], 
+    [
+        (geo.Point(), geo.Point()),
+        (geo.Point(x=3.5), geo.Point(x=3.5)),
+        (geo.Point(y=5.25), geo.Point(y=5.25)),
+        (geo.Point(x=3.5, y=5.25), geo.Point(x=3.5, y=5.25)),
+        (geo.Point("A",3.5,5.25), geo.Point("A",3.5,5.25)),
+    ],
+    ids=[
+        "default point",
+        "x explicit",
+        "y explicit",
+        "x,y explicit",
+        "name,x,y explicit",
+        ]
+)
+def test_eq_ok(p1,p2):
+    assert p1 == p2
+    assert not (p1 != p2)
+
+@pytest.mark.parametrize(
+    ["p1","p2"], 
+    [
+        (geo.Point(), geo.Point("A")),
+        (geo.Point("A"), geo.Point()),
+        (geo.Point("A",3.5,5.25), geo.Point("B",3.5,5.25)),
+        (geo.Point("A",3.5,5.25), geo.Point("A",4.5,5.25)),
+        (geo.Point("A",3.5,5.25), geo.Point("A",3.5,6.25)),
+        (geo.Point("A",3.5,5.25), geo.Point("B",4.5,6.25)),
+        (geo.Point("A",3.5,5.25), 12),
+        (12, geo.Point("A",3.5,5.25)),
+        (geo.Point("A",3.5,5.25), "B"),
+        ("B", geo.Point("A",3.5,5.25)),
+    ],
+    ids=[
+        "name None vs str",
+        "name str vs None",
+        "name different",
+        "x different",
+        "y different",
+        "name,x,y different",
+        "Point vs int",
+        "int vs Point",
+        "Point vs str",
+        "str vs Point",
+
+
+    ])
+def test_eq_ko(p1,p2):
+    assert not(p1 == p2)
+    assert p1 != p2
+
+def test_eq_same():
+    p = geo.Point("A", 1.25, 3.5)
+    assert p == p
+    assert not(p != p)
