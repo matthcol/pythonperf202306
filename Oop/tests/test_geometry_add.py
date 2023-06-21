ae98aba2fa341ae46445cc635777c0a22036446d
matthcol <matthias.colin@gmail.com>
2023-06-21 15:15:17 +0200

    tests for add with match case

--- Oop/tests/test_geometry_add.py
+++ Oop/tests/test_geometry_add.py
@@ -10,21 +10,104 @@ def pointB():
     return geo.Point("B", 1.5, 1.75)
 
 @pytest.fixture
-def tupleB():
+def tupleXY():
     return (1.5, 1.75)
+
+@pytest.fixture
+def tupleNXY():
+    return ("A",1.5, 1.75)
+
+@pytest.fixture
+def listXY():
+    return [1.5, 1.75]
+
+@pytest.fixture
+def listNXY():
+    return ["A",1.5, 1.75]
+
+@pytest.fixture
+def dictX():
+    return {"x":1.5}
+
+@pytest.fixture
+def dictY():
+    return {"y":1.75}
+
+@pytest.fixture
+def dictXY():
+    return {"x":1.5, "y":1.75}
+
+@pytest.fixture
+def dictNXY():
+    return {"name": "A","x":1.5, "y":1.75}
 
 def test_add_point(pointA, pointB):
     res = pointA + pointB 
     assert res.x == 4.0
     assert res.y == 5.0
 
-def test_add_tuple(pointA, tupleB):
-    res = pointA + tupleB
+def test_add_tupleXY(pointA, tupleXY):
+    res = pointA + tupleXY
     assert res.x == 4.0
     assert res.y == 5.0
 
-def test_radd_tuple(pointA, tupleB):
-    res = tupleB + pointA
+def test_add_tupleNXY(pointA, tupleNXY):
+    res = pointA + tupleNXY
+    assert res.x == 4.0
+    assert res.y == 5.0
+
+def test_add_listXY(pointA, listXY):
+    res = pointA + listXY
+    assert res.x == 4.0
+    assert res.y == 5.0
+
+def test_add_listNXY(pointA, listNXY):
+    res = pointA + listNXY
+    assert res.x == 4.0
+    assert res.y == 5.0
+
+def test_add_dictX(pointA, dictX):
+    res = pointA + dictX
+    assert res.x == 4.0
+    assert res.y == 3.25 # pointA.y
+
+def test_add_dictY(pointA, dictY):
+    res = pointA + dictY
+    assert res.x == 2.5 # pointA.x
+    assert res.y == 5
+
+def test_add_dictXY(pointA, dictXY):
+    res = pointA + dictXY
+    assert res.x == 4.0
+    assert res.y == 5.0
+
+def test_add_dictNXY(pointA, dictNXY):
+    res = pointA + dictNXY
+    assert res.x == 4.0
+    assert res.y == 5.0
+
+
+@pytest.mark.parametrize(
+        "scalar,expX,expY",
+        [
+            (2, 4.5, 5.25),
+            (2.25, 4.75, 5.5),
+            (True, 3.5, 4.25), 
+            (False, 2.5, 3.25),
+        ],
+        ids=["int","float",
+             "bool true","bool false",
+        ]
+)
+def test_add_scalar(pointA, scalar, expX, expY):
+    res = pointA + scalar
+    assert res.x == expX
+    assert res.y == expY
+
+
+@pytest.mark.xfail(raises=TypeError)
+def test_radd_tuple(pointA, tupleXY):
+    res = tupleXY + pointA
     assert res.x == 4.0
     assert res.y == 5.0
 
@@ -33,7 +116,30 @@ def test_iadd_point(pointA, pointB):
     assert pointA.x == 4.0
     assert pointA.y == 5.0
     
-def test_iadd_tuple(pointA, tupleB):
-    pointA += tupleB
+def test_iadd_tuple(pointA, tupleXY):
+    pointA += tupleXY
     assert pointA.x == 4.0
     assert pointA.y == 5.0
+
+notAddableDataList = [
+    "A", # cannot add str (for now)
+    (3,4,5,6), # cannot add list with too uch element
+    [3,4,5,6], # cannot add list with too uch element
+    {"key":1} # cannot add dict without x or y key
+] 
+
+@pytest.mark.parametrize(
+    "other", 
+    notAddableDataList
+)
+def test_add_ko_notAddable(pointA, other):
+    with pytest.raises(TypeError):
+        pointA + other
+
+@pytest.mark.parametrize(
+    "other", 
+    notAddableDataList
+)
+def test_iadd_ko_notAddable(pointA, other):
+    with pytest.raises(TypeError):
+        pointA += other
